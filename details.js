a395f0810ce9fa1275f560e7df7aae9e0530d549
usimarc <usimarc@hotmail.com>
2023-05-28 16:04:37 -0300

    screenDetails

---
+++ details.js
@@ -0,0 +1,34 @@
+let card = "";
+
+const parametros = window.location.href;
+var id = parametros.split('?')[1].split('=')[1];
+console.log(id);
+
+function getProdutc() {
+
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      card += `
+      <div class="card style="width: 58rem;">
+      
+        <img src="${data.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h2 class="card-title textoLimite">${data.title}</h2>
+            <div class="rating">
+            <p><strong>Descrição:</strong> ${data.description}</p>
+          </div>
+          <div class="rating">
+            <p><strong>Cassificação:</strong> ${data.rating.rate}</p>
+          </div>
+          <h4 class="card-text"><mark>Valor: R$ ${data.price} </mark></h4>
+        </div>
+      </div>`;
+
+      document.getElementById('amosaic').innerHTML = card;
+    });
+
+};
+
+getProdutc();
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const search = document.getElementById('button-addon2');
 let card = "";
+
+search.addEventListener('click', () => {
+  window.location.href = 'search.html';
+
+})
+
+
+
+function searchProdutcs() {
 fetch('https://fakestoreapi.com/products?limit=9')
   .then(res => res.json())
   .then(data => {
@@ -6,9 +16,10 @@ fetch('https://fakestoreapi.com/products?limit=9')
       console.log(data[i]);
       card += `
       <div class="card">
+      <a href="details.html?id=${data[i].id}">
         <img src="${data[i].image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <a href="#">
+          
             <h6 class="card-title textoLimite">${data[i].title}</h6>
           </a>
           <div class="rating">
@@ -20,5 +31,7 @@ fetch('https://fakestoreapi.com/products?limit=9')
     }
     document.getElementById('mosaic').innerHTML = card;
   });
-  
 
+};
+
+searchProdutcs();
